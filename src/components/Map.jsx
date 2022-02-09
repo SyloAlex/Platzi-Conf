9b19f110120e12c8b6ce5a36080804eef3dbf4f0
Alex Mena <alexkamu.m@gmail.com>
2022-02-09 11:25:19 -0400

    First attempt at using React-Leaflet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import '../styles/components/Map.css'
 
 const Map = () => {
-    const mapStyles = {
-        height: "50vh",
-        width: "100%"
-    }
-    const defaultCenter = {
-        lat: 19.4267261,
-        lng: -99.1718796
-    }
+    const defaultCenter = [19.4267261, -99.1718706]
+
     return (
-        <LoadScript googleMapsApiKey='AIzaSyCnIQImLfqTPszg_hKjMGsUmtkrkghny58'>
-            <GoogleMap
-                MApStyle={mapStyles}
-                zoom={9}
-                center={defaultCenter} >
-                <Marker position={defaultCenter} />
-            </GoogleMap>
-        </LoadScript>
+        <MapContainer center={defaultCenter} zoom={17} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={defaultCenter} />
+        </MapContainer>
     );
 };
 
--- src/containers/PaySuccess.jsx
+++ src/containers/PaySuccess.jsx
@@ -9,7 +9,7 @@ const PaySuccess = () => {
     return (
         <div className="Success">
             <div className="Success-content">
-                <h2>{`${buyer[0].name}! Thank you for your purchase!`}</h2>
+                {/* <h2>{`${buyer[0].name}! Thank you for your purchase!`}</h2> */}
                 <span>Your order will arrive in N days</span>
                 <div className="Success-map">
                     <Map />
